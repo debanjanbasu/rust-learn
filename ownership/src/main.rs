eb50cf12ec2f473e068de92b492ed8f36e2c4146
Debanjan Basu <debanjanbasu2006@gmail.com>
2023-10-04 21:09:59 +1100

    added in ownership example

---
+++ ownership/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    println!("The length of '{}' is {}.", &s1, calculate_length(&s1));
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
